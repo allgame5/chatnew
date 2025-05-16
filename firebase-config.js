d3d476e549f2dcbbebdd40d73b5cfc7e84ff8f1d
Loes <all.game.11king@gmail.com>
2025-05-16 20:02:06 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 // Deine Firebase-Konfiguration hier einfügen:
 const firebaseConfig = {
-  apiKey: "DEIN_API_KEY",
-  authDomain: "DEIN_PROJEKT.firebaseapp.com",
-  projectId: "DEIN_PROJEKT_ID",
-  storageBucket: "DEIN_PROJEKT.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyBNExUxZTluE48Bcimnj1WCYH8p6olFiZU",
+  authDomain: "chatco-fc596.firebaseapp.com",
+  projectId: "chatco-fc596",
+  storageBucket: "chatco-fc596.firebasestorage.app",
+  messagingSenderId: "192318597553",
+  appId: "1:192318597553:web:1603c7f541028b3fe67590",
+  measurementId: "G-6DMGPVC94M"
 };
 
 firebase.initializeApp(firebaseConfig);
